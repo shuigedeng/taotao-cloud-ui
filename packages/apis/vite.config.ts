3118a8f5bc4b43b2b0431ab23e7fc60b30494229
shuigedeng <981376577@qq.com>
2023-02-19 11:43:33 +0800

    修改api

--- packages/apis/vite.config.ts
+++ packages/apis/vite.config.ts
@@ -1,3 +1,11 @@
+/*
+ * @Author: shuigedeng 981376577@qq.com
+ * @Date: 2023-01-28 11:23:41
+ * @LastEditors: shuigedeng 981376577@qq.com
+ * @LastEditTime: 2023-02-03 14:56:53
+ * @FilePath: /taotao-cloud-ui/packages/apis/vite.config.ts
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
 import { defineConfig } from 'vite';
 
 import dts from 'vite-plugin-dts';
